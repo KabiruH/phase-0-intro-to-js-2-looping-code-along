b8a874801f6db014b1b96e4dd6ae9dc02308f9d1
KabiruH <kelkab46@gmail.com>
2022-11-22 22:48:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (string, event){
+  var thisarray = []; 
+  for (let counter = 0; counter < string.length; counter++){ 
+      thisarray.push(`Thank you, ${string[counter]}, for the wonderful ${event} gift!`);
+    }
+  return thisarray;
+}
+function countDown(num){
+  
+  while (num >= 0)
+  {
+      console.log(num)
+      num--;
+  }
+}
